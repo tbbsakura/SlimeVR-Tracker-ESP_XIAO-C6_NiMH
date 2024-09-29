caf6d8f0e61c61cad333b708f7d872400dd86925
tbbsakura <tbbsakura1@gmail.com>
2024-09-29 20:13:06 +0900

    all diffs from main as of Sep 29, 2024

--- src/debug.h
+++ src/debug.h
@@ -69,8 +69,8 @@
 
 // Battery configuration
 #define batterySampleRate 10000
-#define BATTERY_LOW_VOLTAGE_DEEP_SLEEP false
-#define BATTERY_LOW_POWER_VOLTAGE 3.3f // Voltage to raise error
+#define BATTERY_LOW_VOLTAGE_DEEP_SLEEP true
+#define BATTERY_LOW_POWER_VOLTAGE 0.95f // Voltage to raise error
 
 // Send updates over network only when changes are substantial
 // If "false" updates are sent at the sensor update rate (usually 100 TPS)
